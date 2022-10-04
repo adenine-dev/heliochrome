9a066bc1b4b6e8c8724b182d63a63ae43352f33f
Adenine <adenine.dev@gmail.com>
2022-10-04 14:49:36 -0400

    fix rects

--- heliochrome/src/hittables/rect.rs
+++ heliochrome/src/hittables/rect.rs
@@ -15,7 +15,7 @@ impl Rect {
             origin,
             sx,
             sy,
-            normal: sx.cross(sy),
+            normal: sx.cross(sy).normalize(),
         }
     }
 }
@@ -27,8 +27,8 @@ impl Hittable for Rect {
             let t = (self.origin - ray.origin).dot(self.normal) / d;
             if t_min <= t && t <= t_max {
                 let p = self.origin - ray.at(t);
-                let q1m = ((p.dot(self.sx) / self.sx.mag()) * self.sx).mag();
-                let q2m = ((p.dot(self.sy) / self.sy.mag()) * self.sy).mag();
+                let q1m = ((p.dot(self.sx) / self.sx.mag()) * (self.sx / self.sx.mag())).mag();
+                let q2m = ((p.dot(self.sy) / self.sy.mag()) * (self.sy / self.sy.mag())).mag();
                 if 0.0 <= q1m && q1m <= self.sx.mag() && 0.0 <= q2m && q2m <= self.sy.mag() {
                     return Some(Hit::new(ray, t, self.normal));
                 }
@@ -42,12 +42,14 @@ impl Hittable for Rect {
             .origin
             .min(&(self.origin + self.sx))
             .min(&(self.origin + self.sy))
-            .min(&(self.origin + self.sx + self.sy));
+            .min(&(self.origin + self.sx + self.sy))
+            - vec3::splat(0.0001);
         let max = self
             .origin
             .max(&(self.origin + self.sx))
             .max(&(self.origin + self.sy))
-            .max(&(self.origin + self.sx + self.sy));
+            .max(&(self.origin + self.sx + self.sy))
+            + vec3::splat(0.0001);
 
         Some(AABB::new(min, max))
     }
